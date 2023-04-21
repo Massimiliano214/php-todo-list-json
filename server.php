a3f5121af05aa25fe3954d876db2d00b5825a1c6
Massimiliano Fanizza <fanizzamassimiliano@gmail.com>
2023-04-21 15:45:24 +0200

    added dinamic server list

--- server.php
+++ server.php
@@ -1,4 +1,5 @@
 <?php
+    
     $toDoList = [
         ["action" => "Fare la Spesa", "value" => true],
         ["action" => "Cucinare", "value" => false],
@@ -6,15 +7,28 @@
         ["action" => "Dormire", "value" => false],
     ];
     
+    
     if (isset($_POST['itemToAdd'])) {
         $value = $_POST['itemToAdd']['value'] === "true" ? true : false;
         $toDoList[] = [
             'action' => $_POST['itemToAdd']['action'],
             "value" => $value
         ];
+
+
+        $myString = json_encode($toDoList);
+        file_put_contents('database.json', $myString);
     }
     
     header('Content-Type: application/json');
     
     echo json_encode($toDoList);
 ?>
+
+<!--
+if (file_exists('database.json')) {
+        $string = file_get_contents('database.json');
+        $toDoList = json_decode($string, true);
+    } else {
+        $toDoList = [];
+    }
